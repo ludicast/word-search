99699bfb3ce458cb212dee61276bb7cea4a20713
Nate Kidwell <nate@domastic.com>
2024-10-18 03:49:30 -0400

    update

--- types/random.d.ts
+++
@@ -1,3 +0,0 @@
-export type Seeder = (max?: number) => number;
-export declare function seederRandom(seed?: string): Seeder;
-export declare function seededShuffle<T>(array: T[], random?: Seeder): T[];
